55312d1d2a2f1110001127817bf30fb13425b3ab
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-07 09:10:33 +0300

    pass data into the single post component using props

---
+++ components/posts/post-item.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './post-item.module.css';
+
+const PostItem = props => {
+    const { title, image, date, shortText, slug  } = props;
+
+    const readableDate = new Date(date).toLocaleDateString('en-US', {
+        day:'numeric',
+        month:'long',
+        year:'numeric'
+    }) 
+
+    return <li className={styles.post}>
+        <Link href=''>
+            <a>
+                <div className={styles.image}>
+                <Image src={`/images/posts/${slug}/${image}`} alt={title} width={300} height={200}/>
+                </div>
+                <div className={styles.content}>
+                    <h3>{title}</h3>
+                    <time>{readableDate}</time>
+                    <p>{shortText}</p>
+                </div>
+            </a>
+        </Link>
+    </li>
+}
+
+export default PostItem;
