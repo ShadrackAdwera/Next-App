ca354c9a89409e20134506236f5203f61fb97178
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-08 08:23:29 +0300

    display content on the post details page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,20 @@
+import PostHeader from './post-header';
+import styles from './post-content.module.css';
+
+const DUMMY_POST = {
+    title: 'Why Nextjs over React.js',
+    image: 'getting-started-nextjs.png',
+    date: '2021-04-04',
+    content:
+      '[I am an inline-style link to this post](https://www.google.com)',
+    slug: 'why-nextjs-over-reactjs'
+  }
+
+const PostContent = props => {
+    return <article className={styles.content}>
+        <PostHeader title={DUMMY_POST.title} image={`/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`}/>
+        {DUMMY_POST.content}
+    </article>
+}
+
+export default PostContent;
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,7 @@
-const Post = () => {
+import PostContent from '../../components/posts/post-detail/post-content';
 
+const Post = () => {
+    return <PostContent />
 }
 
 export default Post;
