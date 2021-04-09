127cc1635919cbcbdf0fedfc68bcfa49464e3e95
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-09 07:53:25 +0300

    ovewrite markdown image with Nextjs Next Image

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,13 +1,21 @@
 import ReactMakdown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import styles from './post-content.module.css';
 
 const PostContent = props => {
   const { title, content, slug, image} = props.post;
+
+  const customRendered = {
+    image(image) {
+      return <Image src={`/images/posts/${slug}/${image.src}`} alt={image.alt} width={600} height={300} layout='responsive'/>
+    }
+  }
+
     return <article className={styles.content}>
         <PostHeader title={title} image={`/images/posts/${slug}/${image}`}/>
-        <ReactMakdown>
+        <ReactMakdown renderers={customRendered}>
         {content}
         </ReactMakdown>
     </article>
